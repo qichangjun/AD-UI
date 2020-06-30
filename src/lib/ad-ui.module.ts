fc4c35478f499f077a7867369856bc47e91cfeaf
robotQic <sevenqcj@163.com>
2020-06-30 16:17:16 +0800

    修复导出错误问题，将adTreeOption导出

--- src/lib/ad-ui.module.ts
+++ src/lib/ad-ui.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { AdUiComponent } from './ad-ui.component';
-import { AdTreeModule } from './ad-tree/ad-tree.module';
-import { AdPaginationModule } from './ad-pagination/ad-pagination.module';
 
 @NgModule({
   declarations: [AdUiComponent],
   imports: [
-    AdTreeModule,
-    AdPaginationModule
   ],
-  exports: [AdUiComponent,AdTreeModule,AdPaginationModule]
+  exports: []
 })
 export class AdUiModule { }
